fce9c45f3975c1776f22f99a4436d36b7fb26c94
Dawn DeWaay <99098265+DawnDeWaay@users.noreply.github.com>
2026-01-28 10:19:34 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# 023 Navigation Bar
+
+[![023 Navigation Bar](https://img.youtube.com/vi/vqhYGUHHGaA/0.jpg)](https://www.youtube.com/watch?v=vqhYGUHHGaA)
+
+The `index.html` file
+- should add styling so that the list is not indented
+- should add styling so that the list is not padded
+- should add styling so that the list does not have bullet points
+- should add styling so that the anchors are padded with 10px
+- should add styling so that the anchors are not underlined
+- should add styling so that the anchors have black text
+
+Resources:
+- https://www.w3schools.com/css/css_navbar.asp
+- https://www.w3schools.com/css/css_navbar_vertical.asp
+
+
+Copyright &copy; 2023 Knight Moves. All Rights Reserved.
---
+++ test/verify.test.js
@@ -0,0 +1,113 @@
+const http = require("http");
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+const { assert } = require("console");
+
+let server;
+let browser;
+let page;
+
+beforeAll(async () => {
+  server = http.createServer((req, res) => {
+    fs.readFile(__dirname + "/.." + req.url, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200);
+      res.end(data);
+    });
+  });
+
+  server.listen(process.env.PORT || 3000);
+});
+
+afterAll(() => {
+  server.close();
+});
+
+beforeEach(async () => {
+  try {
+    browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu'],
+    });
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/index.html");
+  } catch (error) {
+    console.error("Failed to launch Puppeteer browser:", error);
+    throw error;
+  }
+});
+
+afterEach(async () => {
+  try {
+    await browser.close();
+  } catch (error) {
+    console.error("Failed to close Puppeteer browser:", error);
+  }
+});
+
+describe('the list', () => {
+  it('should not be indented', async () => {
+    const value = await page.$eval('ul', (list) => {
+      let listStyle = window.getComputedStyle(list);
+      return listStyle.getPropertyValue('margin');
+    });
+
+    expect(value).toBe('0px');
+  });
+
+  it('should not be padded', async () => {
+    const value = await page.$eval('ul', (list) => {
+      let listStyle = window.getComputedStyle(list);
+      return listStyle.getPropertyValue('padding');
+    });
+
+    expect(value).toBe('0px');
+  });
+
+  it('should not have bullet points', async () => {
+    const value = await page.$eval('ul', (list) => {
+      let listStyle = window.getComputedStyle(list);
+      return listStyle.getPropertyValue('list-style-type');
+    });
+
+    expect(value).toBe('none');
+  });
+});
+
+describe('the anchors', () => {
+  it('should be padded with 10px', async () => {
+    const display = await page.$eval('a', (anchor) => {
+      let listStyle = window.getComputedStyle(anchor);
+      return listStyle.getPropertyValue('display');
+    });
+
+    const padding = await page.$eval('a', (anchor) => {
+      let listStyle = window.getComputedStyle(anchor);
+      return listStyle.getPropertyValue('padding');
+    });
+
+    expect(display).toBe('block');
+    expect(padding).toBe('10px');
+  });
+
+  it('should not be underlined', async () => {
+    const value = await page.$eval('a', (anchor) => {
+      let listStyle = window.getComputedStyle(anchor);
+      return listStyle.getPropertyValue('text-decoration');
+    });
+
+    expect(value).toContain('none');
+  });
+
+  it('should have black text', async () => {
+    const value = await page.$eval('a', (anchor) => {
+      let listStyle = window.getComputedStyle(anchor);
+      return listStyle.getPropertyValue('color');
+    });
+
+    expect(value).toBe('rgb(0, 0, 0)');
+  });
+});
